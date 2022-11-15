67a86e60b22a3b760f5d6e5553922989360d3584
Issa Charbaji <80466126+issacharbaji@users.noreply.github.com>
2022-11-15 19:41:45 +0200

    Add Terminal Menu

---
+++ Assets/Hacker.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShowMainMenu();      
+    }
+
+    void ShowMainMenu()
+    {
+        Terminal.WriteLine("what would you like to hack into?");
+        Terminal.WriteLine("Press 1 for the local library");
+        Terminal.WriteLine("Press 2 for the police station");
+        Terminal.WriteLine("Press 3 for NASA");
+        Terminal.WriteLine("Enter your selection:");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
