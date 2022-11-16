26c0154ec96b216ad8ccb9392b144f79abd5619e
Issa Charbaji <80466126+issacharbaji@users.noreply.github.com>
2022-11-16 10:48:43 +0200

    Handle the menu

--- Assets/Hacker.cs
+++ Assets/Hacker.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,11 +13,42 @@ public class Hacker : MonoBehaviour
 
     void ShowMainMenu()
     {
-        Terminal.WriteLine("what would you like to hack into?");
+        Terminal.ClearScreen();
+        Terminal.WriteLine("What would you like to hack into?");
         Terminal.WriteLine("Press 1 for the local library");
         Terminal.WriteLine("Press 2 for the police station");
         Terminal.WriteLine("Press 3 for NASA");
         Terminal.WriteLine("Enter your selection:");
+    }
+
+    void OnUserInput(string input)
+    {
+        /*var menu = "menu";*/
+   
+        if ( input == "1")
+        {
+            StartGame(1);
+        }
+        else if( input == "2")
+        {
+            StartGame(2);
+        }
+        else if( input == "3")
+        {
+            StartGame(3);
+        }
+        else if ( input == "menu") { 
+            ShowMainMenu();
+        }
+        else
+        {
+            Terminal.WriteLine("Please choose a valid leve!");
+        }
+    }
+
+    void StartGame(int level)
+    {
+        Terminal.WriteLine("You have choosen level:" + level);
     }
 
     // Update is called once per frame
